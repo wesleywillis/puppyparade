30d62afe9f10624f4954156ca8d5a11b96f74fa0
wesleywillis <haileyw@gmail.com>
2015-06-01 22:05:45 -0500

    trying to tweak by comparing working v14 and non working v15

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150530015750) do
+ActiveRecord::Schema.define(version: 20150418202754) do
 
   create_table "dogs", force: :cascade do |t|
     t.string   "name"
